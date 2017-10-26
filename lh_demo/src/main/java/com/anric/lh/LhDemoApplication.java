b40efce4b4b032886f07ed33816f01f4448d94a7
wuxiaohu <wuxh@tourage.cn>
2017-10-26 11:34:58 +0800

    初始项目-wxh

---
+++ lh_demo/src/main/java/com/anric/lh/LhDemoApplication.java
@@ -0,0 +1,12 @@
+package com.anric.lh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LhDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/anric/AnricDemoApplication.java
@@ -0,0 +1,12 @@
+package com.anric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnricDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnricDemoApplication.class, args);
+	}
+}
